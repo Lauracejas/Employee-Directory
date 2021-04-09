2495e3ff9e29d102e5b2adf6381d0cf522d64a0a
lauracejas <lauracejasmartin94@gmail.com>
2021-04-09 09:01:06 -0400

    create header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Wrapper>
+     <Header />
+   </Wrapper>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const styles = {
+    headerStyle: {
+      background: "darkblue",
+      color: "white",
+      
+    },
+    
+  };
+
+function Header () {
+    return (
+        <div className="jumbotron jumbotronfluid"  style={styles.headerStyle}>
+            <h1 class="d-flex justify-content-center">Employee Directory</h1>
+            <p class="d-flex justify-content-center">Click on each heading to filter or use the search box to narrow your results</p>
+        </div>
+    )
+}
+
+export default Header;
